5791e82a30937a79c31d8166c8b25ef1549bf9ac
DeveloperRooney <han_0427@naver.com>
2021-06-09 14:32:38 +0900

    피보나치 수열

--- src/algorithm/Fibonacci.java
+++ src/algorithm/Fibonacci.java
@@ -1,4 +1,30 @@
 package algorithm;
 
 public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 1;
+        int y = 2;
+
+        int n = 2;
+
+
+        while(true) {
+            int c = a + b;
+            y = y + c;
+            n = n + 1;
+
+            if (n >= 5) {
+                break;
+            }else {
+                a = b;
+                b = c;
+            }
+        }
+
+        System.out.println(y);
+
+    }
 }
